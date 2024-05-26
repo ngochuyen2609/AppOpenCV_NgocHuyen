2fa432cb7b160bd92b5a6e104b71c54a0ee6071b
ngochuyen2609 <166501929+ngochuyen2609@users.noreply.github.com>
2024-05-26 18:16:56 +0700

    CaoThiNgocHuyen update

--- src/main/java/com/example/demo4/FilterImageController.java
+++ src/main/java/com/example/demo4/FilterImageController.java
@@ -1,15 +1,62 @@
 package com.example.demo4;
-import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class FilterImageController extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
+public class FilterImageController {
+
+    private Stage stage;
+    private Image image; 
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        
+    public void setImage (Image image) {
+        this.image = image;
+    }
+
+    public void chooseFilter() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("image-view.fxml"));
+            Parent root = loader.load();
+
+            ImageViewController controller = loader.getController();
+            controller.setStage(stage);
+            controller.setImage(image); // Sử dụng ảnh gần nhất
+            Scene scene = new Scene(root);
+
+            // Thêm CSS
+            scene.getStylesheets().add(getClass().getResource("image.css").toExternalForm());
+            stage.setTitle("Test Filter");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickFilter1(ActionEvent event) {
+        chooseFilter();
+    }
+
+    public void clickFilter2(ActionEvent event) {
+        chooseFilter();
+    }
+
+    public void clickFilter3(ActionEvent event) {
+        chooseFilter();
+    }
+
+    public void clickFilter4(ActionEvent event) {
+        chooseFilter();
     }
 }
+
