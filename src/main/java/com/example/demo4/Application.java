c4edcc2065226c8e74c3d2cdbb1aee92a1e0adff
ngochuyen2609 <166501929+ngochuyen2609@users.noreply.github.com>
2024-05-26 18:16:22 +0700

    CaoThiNgocHuyen update

--- src/main/java/com/example/demo4/Application.java
+++ src/main/java/com/example/demo4/Application.java
@@ -1,7 +1,5 @@
-
 package com.example.demo4;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,25 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
             //Thêm fxml
             FXMLLoader loader =new FXMLLoader(getClass().getResource("hello-view.fxml"));
             Parent root = loader.load();
-
-            // Truyền tham chiếu của stage vào HelloController
-            HelloController controller = loader.getController();
-            controller.setStage(stage);
-
             Scene scene = new Scene(root);
+
+            // Truyền tham chiếu của stage vào MainController
+            MainController controller = loader.getController();
+            controller.setStage(stage);
 
             //thêm css
             scene.getStylesheets().add(getClass().getResource("takeAPhoto.css").toExternalForm());
             scene.getStylesheets().forEach(System.out::println);
 
-            stage.setTitle("Hello!");
+            stage.setTitle("Test Frontend");
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
